772fb9f8fb6fe1093448193f4c88d45f36dc161e
PaivaBentao <113910119+PaivaBentao@users.noreply.github.com>
2022-09-19 10:32:15 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("A GALERA DO BENTÃO É FERA!")
+st.title("Vinicin e sua historia de amor")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("O HOMEM É BARABO")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Pagode")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("com suas novas aventuras o brabo descobriu que há novos horizontes além do mundo valorantico, sua conta na riot gamens ja não é aberta faz uma semana")
